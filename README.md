935a43adfc5fac46aa0a3262fec9f6a759d0107f
NayanMallet <nayan.mallet@ynov.com>
2023-02-09 17:02:39 +0100

    add some infos on class

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 ## Objectifs
 - Créez un simulateur de Zoo, qui fonctionne en tour par tour, chaque tour représente 1 mois :
-• (2 points) Mettez en place un scénario type : 2 couples de tigre, 2 couples d’aigle, 1 coq, 10 poules sur 10 ans
-• (4 points) Mettez en place toutes les classes de la simulation : voir correction du TP le feu dans le Zoo
-• (4 points) Gérez les 4 actions
-• (10 points) Gérez les 10 événements
-• (5 points) Bonus : Faire une interface Qt
+- (2 points) Mettez en place un scénario type : 2 couples de tigre, 2 couples d’aigle, 1 coq, 10 poules sur 10 ans
+- (4 points) Mettez en place toutes les classes de la simulation : voir correction du TP le feu dans le Zoo
+- (4 points) Gérez les 4 actions
+- (10 points) Gérez les 10 événements
+- (5 points) Bonus : Faire une interface Qt
 Rendu par groupe de 2 maxi
 ## Ressources
 - [Données de référence 🔗](https://docs.google.com/spreadsheets/d/1LHXIDm7j5kan1B9-40uPYAEBp8GY4cxpyNoVweuNUr0/edit?usp=sharing)
--- main.cpp
+++ main.cpp
@@ -1,6 +1,130 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // Mettez en place un scénario type : 2 couples de tigre, 2 couples d’aigle, 1 coq, 10 poules sur 10 ans
+    // TODO: 1. Créer une classe Aliment
+        // -> nom
+        // -> Prix / kg
+
+    // TODO: 2 Créer une classe Habitat 🔴
+        // -> nom
+        // -> capacité
+        // -> type d’animal (ex: Tigre, Aigle, Coq, Poule)
+        // -> prix d’achat
+        // -> prix de vente
+        // -> Perte lié à la Surpopulation (50% de chance)
+        // -> Probabilité d'être malade
+            // => vector d'animal (ex: vector<typeAnimal*> nomHabitat(capacitéHabitat))
+
+    // TODO: 3 Créer une classe Stat
+        // Constructeur(*Tigre){
+            // Alimentation(Viande)
+            // Jours avant faim(2)
+            // fin de reproduction(14 ans)
+            // Espérance de vie(25 ans)
+            // if FEMELLE
+                // Alimentation par jour(10kg)
+                // Maturité sexuelle (4 ans)
+                // Ponte / portée(3 individus par portée / max 1 portée tous les 20 mois)
+                // Gestation(3 mois)
+                // Mortalité infantile(33%)
+            // else MALE
+                // Alimentation par jour(12kg)
+                // Maturité sexuelle (6 mois)
+        // }
+
+        // Constructeur(*Aigle) {
+            // Alimentation(Viande)
+            // Jours avant faim(10)
+            // fin de reproduction(14 ans)
+            // Espérance de vie(25 ans)
+            // Maturité sexuelle (4 ans)
+                // if FEMELLE
+                // Alimentation par jour(0.25kg)
+                // Ponte / portée(2 oeuf en Mars)
+                // Gestation(45 jours)
+                // Mortalité infantile(50%)
+            // else MALE
+                // Alimentation par jour(0.30kg)
+        // }
+
+        // Constructeur(*Coq) {
+            // Alimentation(Viande)
+            // Jours avant faim(2)
+            // fin de reproduction(14 ans)
+            // Espérance de vie(25 ans)
+            // if FEMELLE
+                // Alimentation par jour(10kg)
+                // Maturité sexuelle (4 ans)
+                // Ponte / portée(3 individus par portée / max 1 portée tous les 20 mois)
+                // Gestation(3 mois)
+                // Mortalité infantile(33%)
+            // else MALE
+                // Alimentation par jour(12kg)
+                // Maturité sexuelle (6 mois)
+        // }
+
+        // Constructeur(*Poule) {
+            // Alimentation(Viande)
+            // Jours avant faim(2)
+            // fin de reproduction(14 ans)
+            // Espérance de vie(25 ans)
+            // if FEMELLE
+                // Alimentation par jour(10kg)
+                // Maturité sexuelle (4 ans)
+                // Ponte / portée(3 individus par portée / max 1 portée tous les 20 mois)
+                // Gestation(3 mois)
+                // Mortalité infantile(33%)
+            // else MALE
+                // Alimentation par jour(12kg)
+                // Maturité sexuelle (6 mois)
+        // }
+
+
+
+    // TODO: 3. Créer une classe Ianimal 🔴
+        // -> Nom
+        // -> Sexe
+        // -> Age
+
+        // TODO: 1.1 Créer une classe Tigre : public Ianimal
+            // Tigre() -> param
+            // => Stat(SexeAnimal, Age)
+
+
+
+        // TODO: 1.2 Créer une classe Aigle : public Ianimal
+            // Aigle() -> param
+            // => Stat(SexeAnimal, Age)
+            // Remarque : Fidéle
+
+        // TODO: 1.3 Créer une classe Coq : public Ianimal
+            // Coq() -> param
+            // => Stat(SexeAnimal, Age)
+
+        // TODO: 1.4 Créer une classe Poule : public Ianimal
+            // Poule() -> param
+            // => Stat(SexeAnimal, Age)
+            // Remarque : Fidéle
+
+
+
+
+    // 2. Créer une classe Zoo
+        //flemme
+    // 3. Créer une classe Jour
+        // 3.1 Créer une classe Nourriture
+        // 3.2 Créer une classe Reproduction
+        // 3.3 Créer une classe Mort
+    // 4. Créer une classe Annee
+        // 4.1 Créer une classe Mois
+            // 4.1.1 Créer une classe Semaine
+    vector<Tigre*> coupleTigre(2);
+    coupleTigre.push_back(new Tigre("Tigre1"));
+    coupleTigre.push_back(new Tigre("Tigre2"));
+    vector<Aigle*> coupleAigle(2);
+    cout << "Hello, World!" << endl;
     return 0;
 }
